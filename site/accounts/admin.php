5c3da808cc2f46123ea0c5b35e4287c623a39cf5
Daniel Swakman <dswakman@oefenweb.nl>
2014-10-19 16:25:07 +0300

    installed Kirby

---
+++ site/accounts/admin.php
@@ -0,0 +1,11 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: admin
+email: hello@ldaniel.eu
+password: >
+  $2a$10$98PWRc7V/8NdBHjUNyUbZunIRq5tq5kUqb8oDCRYtI1lH0Fj7t08G
+language: en
+role: admin
+token: 0657e8f8536580441022323f38a19fc268eca2a0
+history:
+  - null
