289fd5d4a409021b685b67c2bc9349c043c11668
Bastiaan Marinus van de Weerd <id@bm-w.eu>
2014-11-11 08:46:57 -0300

    Re-implement home page (template).

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,10 +1,10 @@
 <?php snippet('header') ?>
 		<main id="home">
 			<section id="home-intro">
-				<h1>Begeleiding bij letselschade</h1>
+				<h1><?php echo $page->ondertitel() ?></h1>
 				<a href="#home-traject">
 					<blockquote>
-						<p>Mijn expertise ligt bij juridisch advies bij letselschade en mijn passie is het begeleiden van benadeelden in het traject.</p>
+						<?php echo $page->intro()->kirbytext() ?>
 					</blockquote>
 					<button type="button" title="Traject">Traject</button>
 				</a>
@@ -12,31 +12,28 @@
 			<section id="home-traject">
 				<a href="/traject">
 					<blockquote>
-						<p>Hoe verloopt het letselschadetraject?</p>
+						<?php echo $page->traject()->kirbytext() ?>
 					</blockquote>
 					<button type="button" title="Meer info">Meer info</button>
 				</a>
 			</section>
 			<section id="home-diensten">
-				<p>Hierin kunnen we u van dienst zijn</p>
+				<?php echo $page->diensten()->kirbytext() ?>
 				<ul>
 					<li>
-						<h2>Verkeers&shy;letsel</h2>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+						<?php echo $page->dienst_1()->kirbytext() ?>
 					</li>
 					<li>
-						<h2>Bedrijfs&shy;ongevallen</h2>
-						<p>When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+						<?php echo $page->dienst_2()->kirbytext() ?>
 					</li>
 					<li>
-						<h2>Arbeids&shy;ongevallen</h2>
-						<p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.</p>
+						<?php echo $page->dienst_3()->kirbytext() ?>
 					</li>
 				</ul>
 			</section>
 			<section id="home-contact">
 				<div>
-					<span>Neem contact op voor een vrij&shy;blijvend kennismakingsgesprek:</span>
+					<span><?php echo $page->conctact() ?></span>
 					<a href="mailto:info@ldf-advocatuur.nl">Email: info@ldf-advocatuur.nl</a>
 					<a href="tel:+31207371447">Telefoon: 020-737-1447</a>
 					<a href="https://www.linkedin.com/profile/view?id=287560016">LinkedIn: Liane Foe</a>
