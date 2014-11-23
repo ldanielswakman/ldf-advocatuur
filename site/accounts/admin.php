9f56ac8fa57fcb3b9f257fd65d2e7623d47f7d06
Daniel Swakman <dswakman@oefenweb.nl>
2014-11-23 15:58:02 +0200

    Added new user
    role ‘editor’ for Liane

--- site/accounts/admin.php
+++ site/accounts/admin.php
@@ -6,7 +6,10 @@ password: >
   $2a$10$98PWRc7V/8NdBHjUNyUbZunIRq5tq5kUqb8oDCRYtI1lH0Fj7t08G
 language: en
 role: admin
-token: 1a70e8111f3f1d066454c14ad97975c16db7d328
+token: a79927987e4b45e1243516b54dd9452cd91d7baf
 history:
+  - traject
   - contact
   - null
+firstname:
+lastname:
---
+++ site/accounts/liane.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: liane
+firstname: Liane
+lastname: Foe
+email: liane.foe@ldf-advocatuur.nl
+password: >
+  $2a$10$EWZ1VkpSrT08Ptyg3x3DaeUR/SDYNDBeHug6.wKVlkHki2LvE3yWe
+language: en
+role: editor
+firstName: Liane
+lastName: Foe
