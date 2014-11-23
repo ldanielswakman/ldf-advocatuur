2a284584d5b28c2793f9b41808b32c41c73480aa
Daniel Swakman <dswakman@oefenweb.nl>
2014-11-23 15:25:29 +0200

    updated menu styling & responsiveness

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -10,23 +10,28 @@
 		<link rel="stylesheet" type="text/css" media="all" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
 		<?php echo css('assets/css/main.css') ?>
 		<script src="<?php echo url('https://code.jquery.com/jquery-1.11.1.min.js'); ?>"></script>
-		<!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script><![endif]-->
-		<script src="<?php echo url('assets/scripts/scripts.js'); ?>"></script>
+		<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script><![endif]-->
+		<script src="<?php echo url('https://cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.2/fastclick.min.js'); ?>"></script>
+		<script src="<?php echo url('assets/scripts/scripts.js'); ?>"></script>	
+		<script type="text/javascript">
+			$(document).ready(function() {
+				$('#menu-toggle').click(function(e) {
+					e.preventDefault();
+					$('nav').toggleClass('isExpanded');
+				});
+			});
+		</script>
 	</head>
 	<body>
 		<header>
-			<?php snippet('menu') ?>
-			<script id="button-template" type="text/html">
-				<button type="button"></button>
-			</script>
-			<script type="text/javascript">
-				$(function() {
-					$('body > header > nav')
-						.append($($('#button-template').html()))
-							.addClass('collapsed')
-							.on('click', function() {
-								$('header > nav').toggleClass('collapsed');
-							});
-				});
-			</script>
+			<div class="row">
+				<a href="<?php echo $site->url() ?>" class="home" title="<?php echo $site->titel()->html() ?>">
+					<span class="text">Home</span>
+					<img src="<?php echo url('assets/images/logo.svg') ?>" alt="" />
+				</a>
+				<a href="javascript:void(0)" id="menu-toggle">
+				<i class="entypo-menu"></i>
+				</a>
+				<?php snippet('menu') ?>
+			</div>
 		</header>
--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,6 +1,5 @@
 <nav>
-	<a href="<?php echo $site->url() ?>" class="home" title="<?php echo $site->titel()->html() ?>">Home</a>
-	<ul>
+	<ul class="clearfix">
 	<?php foreach($pages->visible() as $p): ?>
 		<li><a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->titel()->html() ?></a></li>
 	<?php endforeach ?>
