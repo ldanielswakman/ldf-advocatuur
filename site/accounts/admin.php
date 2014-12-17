061366d4e8b7f5f8cd4b291943cf35889d54d22d
Daniel Swakman <dswakman@oefenweb.nl>
2014-12-17 10:03:22 +0200

    added local (dev) resources & prevented @import

--- site/accounts/admin.php
+++ site/accounts/admin.php
@@ -6,7 +6,7 @@ password: >
   $2a$10$98PWRc7V/8NdBHjUNyUbZunIRq5tq5kUqb8oDCRYtI1lH0Fj7t08G
 language: en
 role: admin
-token: a79927987e4b45e1243516b54dd9452cd91d7baf
+token: b6db67906f00aacbc281d4463b1d03dad50f9cf6
 history:
   - traject
   - contact
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,18 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 		<title><?php echo $site->titel() ?> | <?php echo $page->titel() ?></title>
 		<meta name="description" content="<?php echo $site->description()->html() ?>">
 		<meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
-		<link rel="stylesheet" type="text/css" media="all" href="//code.ionicframework.com/ionicons/1.5.2/css/ionicons.min.css" />
-		<link rel="stylesheet" type="text/css" media="all" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
-		<?php echo css('assets/css/main.css') ?>
-		<script src="<?php echo url('https://code.jquery.com/jquery-1.11.1.min.js'); ?>"></script>
+
+		<?php
+		// checks if not on localhost, then serves assets from CDN
+		$local = strpos($_SERVER['SERVER_NAME'], 'localhost');
+		if($local === false) : 
+			echo css('//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css');
+			echo css('//fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic');
+			echo css('//fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700italic');
+			echo css('//weloveiconfonts.com/api/?family=entypo');
+			echo js('//code.jquery.com/jquery-1.11.1.min.js');
+			echo js('//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.2/fastclick.min.js');
+		else :
+			echo css('assets/css/lib/bootstrap.min.css');
+			echo css('assets/css/lib/entypo.css');
+			echo js('assets/scripts/lib/jquery.min.js');
+			echo js('assets/scripts/lib/fastclick.min.js');
+		endif;
+
+		echo css('assets/css/main.css');
+		echo js('assets/scripts/scripts.js');
+		?>
 		<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script><![endif]-->
-		<script src="<?php echo url('https://cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.2/fastclick.min.js'); ?>"></script>
-		<script src="<?php echo url('assets/scripts/scripts.js'); ?>"></script>	
+		
 		<script type="text/javascript">
 			$(document).ready(function() {
 				$('#menu-toggle').click(function(e) {
@@ -21,6 +38,7 @@
 				});
 			});
 		</script>
+
 	</head>
 	<body>
 		<header>
